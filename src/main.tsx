614d9ba8fa782d6e6965540f291a70a607e1a431
dltmdrb667 <dltmdrb667@naver.com>
2026-06-21 22:35:57 +0900

    git commit -m "fix: remove react-query-devtools"

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from '@/router'
@@ -19,7 +18,6 @@ createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
 )
